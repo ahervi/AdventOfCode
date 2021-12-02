484bd922f93e2441831b72a8559f0cb6f9664191
Antoine Hervieu <hervieu.antoine@gmail.com>
2021-12-02 14:24:16 +0100

    Add files via upload

---
+++ 2021/1/advent-day1-part1.py
@@ -0,0 +1,8 @@
+file = open(r'C:\Users\I00380\Documents\Advent_of_Code\1\input.txt', "r")
+lines = file.read().splitlines()
+input = [int(numeric_string) for numeric_string in lines]
+counter = 0
+for i in range(1,len(input)):
+    if input[i] > input[i-1]:
+        counter+=1  
+counter
---
+++ 2021/2/advent-day2-part1.py
@@ -0,0 +1,15 @@
+file = open(r'C:\Users\I00380\Documents\Advent_of_Code\2\input.txt', "r")
+lines = file.read().splitlines()
+input = [(string.split(' ')[0], int(string.split(' ')[1])) for string in lines]
+
+
+depth = 0
+horizontal = 0
+for i in range(len(input)):
+    if input[i][0] == 'forward':
+        horizontal += input[i][1]  
+    elif input[i][0] == 'down':
+        depth += input[i][1] 
+    elif input[i][0] == 'up':
+         depth -= input[i][1] 
+depth*horizontal
